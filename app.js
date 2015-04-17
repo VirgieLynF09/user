eed48645713e204a5b0e8a7d0ff15b2568722a9b
VirgieLynF09 <virgielyn_fernandez@yahoo.ca>
2015-04-17 14:02:12 -0400

    port

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+var port= process.env.PORT ||8000;
+app.listen(port);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
